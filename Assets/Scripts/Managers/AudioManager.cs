d064cf37e1d235881c1d752ffb52d49d78e2088d
blaugranaa <aysenur.fcb@gmail.com>
2021-02-26 17:27:04 +0300

    die sound added (will be fixed)

---
+++ Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField]
+    AudioClip dieSound;
+
+    AudioSource audioSource;
+
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void OnEnable()
+    {
+        EventManager.OnCharacterDie.AddListener(PlayDieSound);
+    }
+    public void PlayDieSound()
+    {
+        //audioSource.clip = dieSound;
+        audioSource.PlayOneShot(dieSound);
+    }
+}
--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,19 +1,37 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] GameObject stayButton, changeButton, choiceText;
+    [SerializeField] GameObject stayButton, changeButton, choiceText, tooL8Text;
 
+    bool isButtonPressed = false;
     public List<Character> StayCharacters = new List<Character>();
     public List<Character> ChangeCharacters = new List<Character>();
 
     public void ChooseScreenOpen()
     {  
         EventManager.OnChooseScreeenOpen.Invoke();
-        SetActiveButtons();
+        SetActiveButtons(); 
     }
 
+    public void CheckChoose()
+    {
+        if (!isButtonPressed)
+        {
+            StartCoroutine(TooLateCo(0.1f));
+        }
+    }
+
+    IEnumerator TooLateCo(float time)
+    {
+        yield return new WaitForSeconds(time);
+        //TooLateToChoose();
+        Stay();
+        tooL8Text.SetActive(true);
+    }
+    
     void SetActiveButtons()
     {
         stayButton.SetActive(true);
@@ -23,6 +41,7 @@ public class UIManager : MonoBehaviour
 
     public void Change()
     {
+        isButtonPressed = true;
         Debug.Log("changed");
         EventManager.OnCharacterSurvive.Invoke(ChangeCharacters);
         Time.timeScale = 1;
@@ -39,8 +58,15 @@ public class UIManager : MonoBehaviour
 
     public void Stay()
     {
+        isButtonPressed = true;
         EventManager.OnCharacterSurvive.Invoke(StayCharacters);
         Debug.Log("stayed");
+        SetInactiveButtons();
+        Time.timeScale = 1;
+    }
+
+    public void TooLateToChoose()
+    {
         SetInactiveButtons();
         Time.timeScale = 1;
     }
